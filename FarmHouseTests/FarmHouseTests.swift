fa7b665925d5cd811d7f1fd8499e89aefeb83b94
Matthew Park <mpark2014@gmail.com>
2016-05-19 18:43:18 -0700

    Initial Commit

---
+++ FarmHouseTests/FarmHouseTests.swift
@@ -0,0 +1,36 @@
+//
+//  FarmHouseTests.swift
+//  FarmHouseTests
+//
+//  Created by Matthew Park on 5/19/16.
+//  Copyright © 2016 Matthew Park. All rights reserved.
+//
+
+import XCTest
+@testable import FarmHouse
+
+class FarmHouseTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
